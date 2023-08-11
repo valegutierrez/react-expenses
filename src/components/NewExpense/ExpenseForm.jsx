b99a68666ae57d445a724f233e57afc50c2f353d
Valentina Gutierrez <pori@Valentinas-MacBook-Pro.local>
2023-08-11 14:49:07 -0400

    two-way binding in expense form

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -10,18 +10,18 @@ const ExpenseForm = () => {
 	const [userInput, setUserInput] = useState({
 		enteredTitle:"",
 		enteredAmount:"",
-		enteredDate: "
+		enteredDate: ""
 	});
 
 	const titleChangeHandler = (event) => {
 		// setEnteredTitle(event.target.value);
-		// setUserInput({
-		// 	...userInput,
-		//	 enteredTitle: event.target.value
-		// })
-		setUserInput((prevState) => {
-			return {...prevState, enteredTitle: event.target.value };
-		});
+		setUserInput({
+			...userInput,
+			enteredTitle: event.target.value
+		})
+		// setUserInput((prevState) => {
+		// 	return {...prevState, enteredTitle: event.target.value };
+		// });
 		// Esta alternativa funciona cuando dependemos del state anterior
 	};
 	
@@ -41,20 +41,42 @@ const ExpenseForm = () => {
 		})
 	};
 
+	// const inputChangeHandler = (identifier, value) => {
+	//	if (identifier === "title") {
+	//		setEnteredTitle(value);
+	//	} else if (identifier === "date") {
+	//		setEnteredDate(value);
+	// 	} else {
+	//		setEnteredAmount(value);
+	// 	}
+	// }; Otra alternativa para hacer una sola arrow function que funcione en los tres botones
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+		const expenseData = {
+			title: userInput.enteredTitle,
+			amount: userInput.enteredAmount,
+			date: new Date(userInput.enteredDate)
+		}
+
+		console.log(expenseData);
+		setUserInput({enteredTitle: "", enteredAmount: "", enteredDate: ""});
+	};
+
 	return (
-		<form>
+		<form onSubmit={submitHandler}>
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" onChange={titleChangeHandler} />
+					<input type="text" value={userInput.enteredTitle} onChange={titleChangeHandler} />
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
-					<input type="number" min="0.01" step="0.01" />
+					<input type="number" value={userInput.enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
 				</div>
 				<div className="new-expense__control">
 					<label>Date</label>
-					<input type="date" min="2019-01-01" max="2022-12-31"/>
+					<input type="date" value={userInput.enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
 				</div>
 				<div className="new-expense__actions">
 					<button type="submit">Add Expense</button>
